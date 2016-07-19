765783cdcd5e9806675cf74739cb1473678c0091
親川将輝 <c15202@c15202.it-college.local>
2016-07-19 12:25:24 +0900

    Initial commit

---
+++ fibo.py
@@ -0,0 +1,15 @@
+#フィボナッチ数モジュール
+def fib(n):
+    a,b=0,1
+    while b<n:
+        print(b,end='')
+        a,b=b,a+b
+
+
+def fib2(n):
+    resuld=[]
+    a,b=0,1
+    while b<n:
+        result.append(b)
+        a,b=b,a+b
+    return result
